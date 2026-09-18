8f8f993cd1d2d6ae3c9495f35d22865a3f6499cf
Wilson Su <wilsonsu2008@hotmail.com>
2026-09-17 23:21:00 -0400

    init: init

---
+++ Game1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using NeoGameLib;
+
+namespace NeoGameLibProjectTemplate;
+
+public class Game1 : NeoGame
+{
+    protected override void OnInitialize()
+    {
+        // TODO: Add your initialization logic here
+    }
+
+    protected override void OnLoadContent()
+    {
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void OnUpdate(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+    }
+
+    protected override void OnDraw(SpriteBatch spriteBatch, GameTime gameTime)
+    {
+        // TODO: Add your drawing code here
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new NeoGameLibProjectTemplate.Game1();
+game.Run();
